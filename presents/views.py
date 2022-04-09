0a55c1e10018dfd8cdca65d502d8ba7c6cfeb7f4
silvianjoki <10silvianjoki@gmail.com>
2022-04-09 20:57:05 +0300

    add path for projects

--- presents/views.py
+++ presents/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponseRedirect
+from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import redirect, render
 from .models import Profile, Project, Vote
 from django.contrib.auth.decorators import login_required
@@ -6,6 +6,7 @@ from .forms import CreateProfileForm, RateProjectForm, CreateProjectForm
 from .email import send_welcome_email
 from django.http import JsonResponse
 from rest_framework import status
+from django.contrib.auth.models import User
 
 
 
@@ -33,4 +34,87 @@ def email(request):
     send_welcome_email(name, email)
     return redirect(create_profile)
     
+@login_required(login_url='/accounts/login/')
+def home(request):
+    projects = Project.display_all_projects()
+    projects_scores = projects.order_by('-average_score')
+    highest_score = None
+    highest_votes = None
+    if len(projects) >= 1:
+        highest_score = projects_scores[0]
+        votes = Vote.get_project_votes(highest_score.id)
+        highest_votes = votes[:3]   
+    
+    return render(request, 'home.html', {'projects': projects, 'projects_scores':projects_scores, 'highest_score':highest_score, 'highest_votes':highest_votes})
 
+@login_required(login_url='/accounts/login/')
+def profile(request, profile_id):
+    try:
+        user = User.objects.get(pk = profile_id)
+        profile = Profile.objects.get(user = user)
+        title = profile.user.username
+        projects = Project.get_user_projects(profile.id)
+        projects_count = projects.count()
+        votes= []
+        for project in projects:
+            votes.append(project.creator_score)
+        total_votes = sum(votes)
+        average = 0
+        if len(projects)> 1:
+            average = total_votes / len(projects)
+    except Profile.DoesNotExist:
+        raise Http404()    
+    return render(request, 'user/profile.html', {'profile':profile, 'title': title, 'projects':projects, 'projects_count':projects_count, 'votes':total_votes, 'average': average})
+
+@login_required(login_url='/accounts/login/')
+def project ( request, project_id):
+    form = RateProjectForm
+    title = project.name.title() + " | slyAwwards"
+    project = Project.objects.get(pk = project_id)
+    votes = Vote.get_project_votes(project_id)
+    total_votes= votes.count()
+    voted=False
+    
+    voters_list =[]
+    average_list = []
+    design_list = []
+    usability_list = []
+    creativity_list = []
+    content_list = []
+    for vote in votes:
+        voters_list.append(vote.voter.id)
+        average_summation = vote.design + vote.creativity + vote.content + vote.usability
+        average = average_summation/3
+        average_list.append(average)
+        content_list.append(vote.content)
+        creativity_list.append(vote.creativity)
+        design_list.append(vote.design)
+        usability_list.append(vote.usability)
+        
+        try:
+            user = User.objects.get(pk = request.user.id)
+            profile = Profile.objects.get(user = user)
+            voter = Vote.get_project_voters(profile)
+            voted = False
+            if request.user.id in voters_list: 
+                voted = True
+        except Profile.DoesNotExist:
+            voted = False
+            
+        if len(average_list) > 0:
+            average_score = sum(average_list) / len(average_list)
+            project.average_score = average_score
+            project.save()
+            
+        if total_votes != 0:
+            creator_design = sum(design_list) / total_votes
+            creator_creativity = sum(creativity_list)/ total_votes
+            creator_content = sum(content_list) / total_votes
+            creator_usability = sum(usability_list) / total_votes 
+            project.creator_design = creator_design
+            project.creator_content =creator_content
+            project.creator_usability = creator_usability
+            project.creator_creativity = creator_creativity
+            project.save() 
+        
+    return render(request, 'project/project.html', {'title':title, 'form':form, 'project':project, 'votes':votes, 'total_votes':total_votes, 'voted':voted})
